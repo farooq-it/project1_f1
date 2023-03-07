3b2a48cedf2ef9d019e9aee982d3ea03004d9bda
farooq-it <farooqgithub@gmail.com>
2023-03-07 19:20:49 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def f1_app1(request):
+    return HttpResponse('saripoindhi')
+
+def f2_app1(request):
+    return HttpResponse('rowdy baby')
---
+++ app2/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def f1_app2(request):
+    return HttpResponse('<h1>endho ee jeevitam</h1>')
+
+
+
+def f2_app2(request):
+    return HttpResponse('<h1>whatsup rowdy boys</h1>')
